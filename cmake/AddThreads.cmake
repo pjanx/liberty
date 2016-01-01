b5724a654a4aadeabcfa3de3847768239931c687
Přemysl Janouch <p.janouch@gmail.com>
2016-01-01 19:15:18 +0100

    Compile tests with pthreads support

---
+++ cmake/AddThreads.cmake
@@ -0,0 +1,23 @@
+# Public Domain
+
+# We're looking for pthreads only, while preferring the -pthread flag
+set (CMAKE_THREAD_PREFER_PTHREAD ON)
+set (THREADS_PREFER_PTHREAD_FLAG ON)
+find_package (Threads)
+
+# Prepares the given target for threads
+function (add_threads target)
+	if (NOT Threads_FOUND OR NOT CMAKE_USE_PTHREADS_INIT)
+		message (FATAL_ERROR "pthreads not found")
+	endif (NOT Threads_FOUND OR NOT CMAKE_USE_PTHREADS_INIT)
+
+	if (THREADS_HAVE_PTHREAD_ARG)
+		set_property (TARGET ${target} PROPERTY
+			COMPILE_OPTIONS "-pthread")
+		set_property (TARGET ${target} PROPERTY
+			INTERFACE_COMPILE_OPTIONS "-pthread")
+	endif (THREADS_HAVE_PTHREAD_ARG)
+	if (CMAKE_THREAD_LIBS_INIT)
+		target_link_libraries (${target} "${CMAKE_THREAD_LIBS_INIT}")
+	endif (CMAKE_THREAD_LIBS_INIT)
+endfunction (add_threads)
